cb89f7c81da761b042f858dee21e5c4f170c5d91
michal <michkro685@student.polsl.pl>
2019-11-09 10:20:48 +0100

    + add config file with connection properties + add database connector

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from models import *
 
 app = Flask(__name__)
 
@@ -7,13 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
-
-
-@app.route('/test')
-def test():
-    category = Category(1, 1, "name")
-    return category.name
 
 
 if __name__ == '__main__':
